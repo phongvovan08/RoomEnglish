a8ecd1774bb2f7c687c85167688cf87bb1695cba
Phong VO <phongvovan08@gmail.com>
2025-10-13 23:32:19 +0700

    remove InitialiseDatabaseAsync

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    await app.InitialiseDatabaseAsync();
+   // await app.InitialiseDatabaseAsync();
 }
 else
 {
