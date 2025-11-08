80708ba813c4b54d9229b59c1622a1515016a711
Phong VO <phongvovan08@gmail.com>
2025-11-08 19:16:43 +0700

    Change speed number

--- src/Web/ClientApp/src/modules/vocabulary/components/DictationCard.vue
+++ src/Web/ClientApp/src/modules/vocabulary/components/DictationCard.vue
@@ -47,12 +47,12 @@
               v-model="playbackSpeed" 
               class="speed-select"
             > 
-             <option :value="0.5">0.5x (Slow)</option>
-              <option :value="0.75">0.75x</option>
+             <option :value="0.5">0.5x</option>
+              <option :value="0.6">0.6x</option>
+              <option :value="0.7">0.7x </option>
+              <option :value="0.8">0.8x</option>
+              <option :value="0.9">0.9x</option>
               <option :value="1">1x (Normal)</option>
-              <option :value="1.25">1.25x</option>
-              <option :value="1.5">1.5x (Fast)</option>
-              <option :value="2">2x</option>
              </select>
           </div>
         </div>
